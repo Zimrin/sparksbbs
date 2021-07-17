f4bce242739e7b393b39968a82622e9a23c65e35
Zimrin <simrin2002@gmail.com>
2021-07-17 20:22:35 +0530

    Update config.php

--- include/config.php
+++ include/config.php
@@ -1,9 +1,9 @@
 <?php
 
 $host = "localhost";
-$username = "root";
-$password = "";
-$database = "bankcustomers";
+$username = "id17264854_root";
+$password = "Qwerty123!@#";
+$database = "id17264854_bankcustomers";
 
 $sql = mysqli_connect($host, $username, $password, $database) or die('Could not connect');
 
